9928ecbb1b765f783cd19f0a9e9e1d8314918ffc
Harish Saini <harish@fueled.com>
2017-02-02 18:09:56 +0530

    basic ui setup

---
+++ Restaurants/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  Restaurants
+//
+//  Created by Harish Saini on 02/02/17.
+//  Copyright © 2017 Fueled. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+	static var identifier = "RestaurantTableViewCellIdentifier"
+
+	@IBOutlet weak var titleLabel: UILabel!
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+	
+	func configure(restaurant: String) {
+		titleLabel.text = restaurant.capitalized
+	}
+}
--- Restaurants/ViewController.swift
+++ Restaurants/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+	
+	fileprivate var dataSource = ["test", "test", "test", "test", "test", "test", "test", "test"]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		configureTableView()
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	private func configureTableView() {
+		tableView.dataSource = self
+		tableView.delegate = self
 	}
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return dataSource.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: RestaurantTableViewCell.identifier, for: indexPath) as! RestaurantTableViewCell
+		let restaurant = dataSource[indexPath.row]
+		cell.configure(restaurant: restaurant)
+		return cell
+	}
+}
+
+extension ViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let restaurant = dataSource[indexPath.row]
+		print("Selected Restaurant : \(restaurant)")
+	}
+}
