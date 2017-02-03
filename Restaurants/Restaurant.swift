6993375a4e39fc5eda8444e29ac0d1d31435b9bc
Harish Saini <harish@fueled.com>
2017-02-03 17:31:21 +0530

    fix

--- Restaurants/Restaurant.swift
+++ Restaurants/Restaurant.swift
@@ -18,29 +18,13 @@ struct Restaurant {
 		guard let data = data else {
 			return
 		}
-		if let venue = data["venue"] as? [String: Any], let name = venue["name"] as? String {
-			self.name = name
+		if let venue = data["venue"] as? [String: Any] {
+			if let name = venue["name"] as? String {
+				self.name = name
+			}
+			if let url = venue["url"] as? String {
+				self.url = url
+			}
 		}
-//		
-//		if let plentiAppVirtualCardUri = data["plenti_app_virtual_card_uri"] as? String {
-//			self.plentiAppVirtualCardUri = plentiAppVirtualCardUri
-//		}
-//		if let plentiAppStoreUrl = data["plenti_app_store_url"] as? String {
-//			self.plentiAppStoreUrl = plentiAppStoreUrl
-//		}
-//		if let message = data["message"] as? String {
-//			self.message = message
-//		}
-//		if let appDisabled = data["app_disabled"] as? String {
-//			self.appDisabled = appDisabled == "Y"
-//		}
-//		
-//		if let connectWellnessPlentiURL = data["plentiquicklinks_url"] as? String {
-//			self.connectWellnessPlentiURL = connectWellnessPlentiURL
-//		}
-//		
-//		if let finishLinkingPlentiURL = data["plenti_mobile_site_url"] as? String {
-//			self.finishLinkingPlentiURL = finishLinkingPlentiURL
-//		}
 	}
 }
